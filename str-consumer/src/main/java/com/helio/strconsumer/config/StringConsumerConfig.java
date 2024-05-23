5b9b3cf2d1ec9b609d50e50229f01ff808e752be
Helio Ortega <helio.ortega.neto@gmail.com>
2024-05-23 20:28:35 -0300

    Implementando Record Interceptor

--- str-consumer/src/main/java/com/helio/strconsumer/config/StringConsumerConfig.java
+++ str-consumer/src/main/java/com/helio/strconsumer/config/StringConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.helio.strconsumer.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -9,9 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.RecordInterceptor;
 
 import java.util.HashMap;
 
+@Log4j2
 @RequiredArgsConstructor
 @Configuration
 public class StringConsumerConfig {
@@ -21,21 +24,38 @@ public class StringConsumerConfig {
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         var configs = new HashMap<String, Object>();
-        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                properties.getBootstrapServers());
-        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
-
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return new DefaultKafkaConsumerFactory<>(configs);
     }
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> strContainerFactory(
-            ConsumerFactory<String, String> consumerFactory) {
+            ConsumerFactory<String, String> consumerFactory
+    ) {
         var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
         factory.setConsumerFactory(consumerFactory);
         return factory;
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> validMessageContainerFactory(
+            ConsumerFactory<String, String> consumerFactory
+    ) {
+        var factory = new ConcurrentKafkaListenerContainerFactory<String, String>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setRecordInterceptor(validMessage());
+        return factory;
+    }
+
+    private RecordInterceptor<String, String> validMessage() {
+        return record -> {
+            if(record.value().contains("Teste")) {
+                log.info("Posssui a palavra Teste");
+                return record;
+            }
+            return record;
+        };
     }
 }
